af7c8093e119e07e89a1c0e4be92dadf85447205
ueno <ueno.hub@gmail.com>
2024-07-03 15:43:28 +0900

    パーシャルプリレンダリングの実装
    Link で指定されたルートをプリレンダリングしたいときはこの設定をするだけでOK
    https://nextjs.org/learn/dashboard-app/partial-prerendering

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,8 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 
+// Next.js アプリの PPR(パーシャルプリレンダリング) を有効にします
+export const experimental_ppr = true;
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">

f3748a6451b0886bcbf774bd5e5ddf689e41876c
ueno <ueno.hub@gmail.com>
2024-07-04 11:46:20 +0900

    form のバリデーション
    ・edit-form.tsxコンポーネントにuseActionStateを追加
    ・zod でフォームのバリデーションとエラー時のラベルを指定
    ・create-form.tsx の aria ラベルについて(アクセシビリティを向上)
    　- aria-describedby="customer-error"：これはselect要素とエラーメッセージコンテナとの関係を確立します。これは、id="customer-error"のコンテナがselect要素を記述していることを示します。スクリーン・リーダは、ユーザがエラーを通知するためにセレクト・ボックスと対話するときに、この説明を読みます。
    　- id="customer-error"：このid属性は、セレクト入力のエラーメッセージを保持する HTML 要素を一意に識別します。これは、aria-describedbyが関係を確立するために必要です。
    　- aria-live="polite"：div内のエラーが更新されたとき、スクリーン・リーダーはユーザーに丁寧に通知する必要があります。コンテンツが変更されたとき（たとえば、ユーザがエラーを修正したとき）、スクリーン・リーダーはこれらの変更をアナウンスしますが、ユーザの邪魔にならないように、ユーザがアイドルであるときに限ります。

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,23 +4,50 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { error } from 'console';
+
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'お客様を選択してください。',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: '$0より大きい金額を入力してください。' }), // .gt()関数で常に0より大きい金額を求めることをZodに伝える
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: '請求書のステータスを選択してください。'
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData) { // useActionStateフックから渡された状態を含みます。prevState は使いませんが必須のPropsです
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'フィールドがありません。請求書の作成に失敗しました',
+    };
+  }
+
+  // データベースに挿入するデータを準備する
+  const { customerId, amount, status } = validatedFields.data;
+
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
@@ -30,8 +57,9 @@ export async function createInvoice(formData: FormData) {
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
+    // データベースエラーが発生した場合より具体的なエラーを返す
     return {
-      message: 'Database Error: Failed to Create Invoice.',
+      message: 'データベースエラーです：請求書の作成に失敗しました。',
     };
   }
 
@@ -42,12 +70,22 @@ export async function createInvoice(formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'フィールドがありません。請求書の更新に失敗しました。',
+    }
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
+
   const amountInCents = amount * 100;
   try {
     await sql`
@@ -56,7 +94,7 @@ export async function updateInvoice(id: string, formData: FormData) {
       WHERE id = ${id}
     `;
   } catch (error) {
-    return { message: 'Database Error: Failed to Update Invoice.' };
+    return { message: 'データベースエラーです：請求書の更新に失敗しました。' };
   }
 
   revalidatePath('/dashboard/invoices');

fc170c48c9a5de5ce48abae809d9f0278e18bddd
ueno <ueno.hub@gmail.com>
2024-07-04 13:54:25 +0900

    typo

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -13,7 +13,7 @@ import { createInvoice, State } from '@/app/lib/actions';
 import { useActionState } from 'react';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
-  const initialState: State = { message: null, error: {}};
+  const initialState: State = { message: null, errors: {}};
   const [state, formAction] = useActionState(createInvoice, initialState);
 
   return (

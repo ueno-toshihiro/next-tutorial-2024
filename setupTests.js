02906b06c018a2d145eebc13fe636ead4785eb5a
ueno <ueno.hub@gmail.com>
2024-07-06 06:09:57 +0900

    coverage 時に google font が認識されないため mock を setupTest.js へ移動
    next/font/google の Inter などが function として認識されないため、mock を個別ページテストから setupTest.js へ移動した

--- setupTests.js
+++ setupTests.js
@@ -1,1 +1,15 @@
 import '@testing-library/jest-dom';
+import { vi } from 'vitest';
+
+vi.mock('next/font/google', () => ({
+  Inter: () => ({
+    style: {
+      fontFamily: 'mocked',
+    },
+  }),
+  Lusitana: () => ({
+    style: {
+      fontFamily: 'mocked',
+    },
+  }),
+}))

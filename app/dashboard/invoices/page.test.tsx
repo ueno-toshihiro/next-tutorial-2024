c46620443a355c4e926f087809e4f75b3d8c42c1
ueno <ueno.hub@gmail.com>
2024-07-07 18:20:48 +0900

    next/navigation の関数がテスト時にエラーを出すため mock 処理
    - next/navigation 関数の mock 対応
    - fetch データの mock 対応

---
+++ app/dashboard/invoices/page.test.tsx
@@ -0,0 +1,48 @@
+import { render, screen } from '@testing-library/react';
+import { expect, test, describe, vi, beforeEach, afterEach } from 'vitest';
+import Page from './page';
+import * as fetchFunctions from '@/app/lib/data';
+import * as navigation from 'next/navigation';
+
+const mockData = {
+  searchParams: {
+    query: 'query',
+    page: '1',
+  },
+};
+
+// 子孫コンポーネントの next/navigation の関数軍がエラーを起こすため、mock する
+vi.mock("next/navigation", async (importOriginal) => {
+  const actual = await importOriginal() as typeof navigation;
+  return {
+    ...actual,
+    useRouter: () => ({
+      replace: vi.fn(),
+    }),
+    usePathname() {
+      return '/dashboard/invoices';
+    },
+    useSearchParams: () => {
+      return {
+        get: () => '1',
+      };
+    },
+  }
+});
+
+describe('Page', () => {
+  beforeEach(() => {
+    vi.spyOn(fetchFunctions, 'fetchInvoicesPages').mockReturnValue(new Promise((resolve) => resolve(1)));
+  });
+  afterEach(() => {
+    vi.restoreAllMocks()
+  });
+
+  test('should render invoices page', async () => {
+    const {debug} = render(await Page(mockData));
+    console.log(debug());
+    expect(
+      screen.getByRole("heading", { level: 1, name: 'Invoices'}))
+      .toBeDefined();
+  });
+});
